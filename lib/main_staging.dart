ffe32bf2ab0bb4596b35080a99e87207ab4abeca
ihorklovan <klyovan88@gmail.com>
2023-10-23 21:59:16 +0200

    chore: add same App for all flavours

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,6 +1,15 @@
+import 'package:flutter/material.dart';
 import 'package:git_explorer/app/app.dart';
 import 'package:git_explorer/bootstrap.dart';
+import 'package:git_explorer/injection.dart';
+import 'package:git_explorer/router.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
-  bootstrap(() => const App());
+  WidgetsFlutterBinding.ensureInitialized();
+  configureDependencies();
+
+  final GoRouter _router = router();
+
+  bootstrap(() => App(routerConfig: _router));
 }
